f1fc73ec5f91315710493b7696a5d2f1da786978
VladislavsJacina <118118069+VladislavsJ@users.noreply.github.com>
2023-03-12 08:16:11 +0300

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,5 +1,4 @@
 # python3
-# python3
 
 import sys
 import threading
@@ -8,25 +7,34 @@ def build_heap(data):
     swaps = []
     temp=0
     flag=False
+    datalen = len(data)
     notleaf = int(len(data)/2-1)
+    cycle =0
     while not flag:
         
         if notleaf>=0:
-            if data[notleaf]>data[notleaf*2+1] or data[notleaf]>data[notleaf*2+1]:
-                if data[notleaf*2+2] > data[notleaf*2+1]:
-                    
-                    swaps.append((notleaf,notleaf*2+1))
-                    temp = data[notleaf]
-                    data[notleaf] = data[notleaf*2+1]
-                    data[notleaf*2+1] = temp
-                else:
-                    
+            if data[notleaf]>data[notleaf*2+1] or (datalen > notleaf*2+2 and data[notleaf]>data[notleaf*2+2]):
+                leftleaf=data[notleaf*2+1]
+                if datalen > notleaf*2+2:
+                    rightleaf=data[notleaf*2+2]
+                else: rightleaf=-1
+                if not rightleaf == -1 and leftleaf>rightleaf:
                     swaps.append((notleaf,notleaf*2+2))
                     temp = data[notleaf]
                     data[notleaf] = data[notleaf*2+2]
                     data[notleaf*2+2] = temp
+                    
+                else: 
+                    swaps.append((notleaf,notleaf*2+1))
+                    temp = data[notleaf]
+                    data[notleaf] = data[notleaf*2+1]
+                    data[notleaf*2+1] = temp
                 notleaf-=1
-                continue
+                cycle+=1
+                if cycle ==50000:
+                    flag = True
+                continue    
+
             if notleaf == 0: flag = True
         else: notleaf = int(len(data)/2-1)        
                 
@@ -48,17 +56,19 @@ def main():
     # input from keyboard
     print("I or F")
     StrOrFile = input()
-    print("n number:")
-    n = int(input())
+
     if StrOrFile[0] == 'F':
         file_name = input()
-        text1 = (open( sys.path[0]+ "/test/" + file_name, "r").read()).split("\n")
-        data=list(map(int, text1[0].split()))
+        text1 = (open( sys.path[0]+ "/tests/" + file_name, "r").read()).split("\n")
+        n = text1[0]
+        data=list(map(int, text1[1].split()))
     elif StrOrFile[0] == 'I':
+        print("n number:")
+        n = int(input())
         data = list(map(int, input().split()))
 
     # checks if lenght of data is the same as the said lenght
-    assert len(data) == n
+    assert len(data) == int(n)
 
     # calls function to assess the data 
     # and give back all swaps
@@ -73,6 +83,7 @@ def main():
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
+    print(len(swaps))
 
 
 if __name__ == "__main__":
